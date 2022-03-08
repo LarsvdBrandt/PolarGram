2049401a9c3bbf3393f97f9a431d6c942f75e173
LarsvdBrandt <Lars.vandenbrandt@me.com>
2022-03-08 16:29:38 +0100

    navbar fix hamburger

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,51 +3,42 @@ import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import * as Icon from 'react-bootstrap-icons';
 
+import { Nav,Container,Navbar } from 'react-bootstrap';
 
 
-function Navbar() {
+
+function Navbarr() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <Link to="/" className="navbar-title">
-        <img className="navbar-Logo" src={polargramlogo} alt="Logo" />
-        PolarGram
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <div>
+      <Navbar collapseOnSelect expand="lg">
+        <Container>
+          <Link to="/" className="navbar-title">
+            <img className="navbar-Logo" src={polargramlogo} alt="Logo" />
+            PolarGram
+          </Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link > 
+              <div className="input-group">
+                <input type="text" className="search-click" name="" placeholder="Search.." /> 
+                <div className="input-group-btn">
+                  <button className="btn btn-default nav-link" type="submit" ><Icon.Search /></button>
+                </div>
+              </div>
+            </Nav.Link>
+            {/* <Nav.Link > <Link to='/Search' className="nav-link"><Icon.Search /> </Link> </Nav.Link> */}
+            <Nav.Link > <Link to='/NewPost'className="nav-link"><Icon.PlusSquare /> </Link></Nav.Link>
+            <Nav.Link > <Link to='/Login'className="nav-link">Login </Link></Nav.Link>
+          </Nav>
         
-        <ul className="navbar-nav ml-auto">
-        <li className="nav-item active">
-            <Link to="/Search" className="nav-link text-uppercase ">
-              <Icon.Search />
-            </Link>
-          </li>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-          <li className="nav-item active">
-            <Link to="/NewPost" className="nav-link text-uppercase">
-              <Icon.PlusSquare />
-            </Link>
-          </li>
 
-          <li className="nav-item active">
-            <Link to="/Login" className="nav-link text-uppercase navbar-rechts">
-              Login
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    </div>
   );
 }
 
-export default Navbar;
+export default Navbarr;

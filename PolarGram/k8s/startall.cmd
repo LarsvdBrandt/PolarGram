5834794b0f58b70c904dbccd55489a1fa9549682
LarsvdBrandt <Lars.vandenbrandt@me.com>
2022-05-24 16:03:48 +0200

    add start and stop file

---
+++ PolarGram/k8s/startall.cmd
@@ -0,0 +1,13 @@
+kubectl apply -f .\frontend-np-service.yaml
+
+kubectl apply -f .\frontend-deploy.yaml
+
+kubectl apply -f .\servicepost-deploy.yaml
+
+kubectl apply -f .\servicecomment-deploy.yaml
+
+kubectl apply -f .\imageapi-deploy.yaml
+
+kubectl apply -f .\apigateway-deploy.yaml
+
+kubectl get services
---
+++ PolarGram/k8s/stopall.cmd
@@ -0,0 +1,13 @@
+kubectl delete -f .\frontend-np-service.yaml
+
+kubectl delete -f .\frontend-deploy.yaml
+
+kubectl delete -f .\servicepost-deploy.yaml
+
+kubectl delete -f .\servicecomment-deploy.yaml
+
+kubectl delete -f .\imageapi-deploy.yaml
+
+kubectl delete -f .\apigateway-deploy.yaml
+
+kubectl get services
